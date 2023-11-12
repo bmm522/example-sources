449ce4688bb7d917e3b45106f80d061bd9506d1b
bmm522 <rlawldls11@naver.com>
2023-11-13 00:52:25 +0900

    test : 줄맞춤 테스트

--- jwt-security-example/src/main/java/com/example/jwtsecurity/auth/jwt/token/Auth0TokenIssuer.java
+++ jwt-security-example/src/main/java/com/example/jwtsecurity/auth/jwt/token/Auth0TokenIssuer.java
@@ -12,34 +12,39 @@ import org.springframework.stereotype.Component;
 @Component
 public class Auth0TokenIssuer implements TokenIssuer {
 
-	@Override
-	public Token makeToken (final TokenMetadata tokenMetadata) {
-		return Token.of(makeTokenString(tokenMetadata));
-	}
+  @Override
+  public Token makeToken(final TokenMetadata tokenMetadata) {
 
-	private String makeTokenString (TokenMetadata tokenMetadata) {
-		Builder builder = JWT.create()
-		  .withSubject(tokenMetadata.subject())
-		  .withExpiresAt(getExpiresAt(tokenMetadata));
-        
-        addClaimIfPresent(builder, tokenMetadata);
+    return Token.of(makeTokenString(tokenMetadata));
+  }
 
-		return builder.sign(Algorithm.HMAC256(tokenMetadata.secret()));
-	}
+  private String makeTokenString(TokenMetadata tokenMetadata) {
 
-	private Date getExpiresAt (TokenMetadata tokenMetadata) {
-		return tokenMetadata.getExpiresAtOfDateType(LocalDateTime.now());
-	}
+    Builder builder = JWT.create()
+      .withSubject(tokenMetadata.subject())
+      .withExpiresAt(getExpiresAt(tokenMetadata));
 
-	private void addClaimIfPresent (Builder builder, TokenMetadata tokenMetadata) {
-		if (isExistClaim(tokenMetadata)) {
-			builder.withClaim(tokenMetadata.claim(), tokenMetadata.userKey());
-		}
+    addClaimIfPresent(builder, tokenMetadata);
 
-	}
+    return builder.sign(Algorithm.HMAC256(tokenMetadata.secret()));
+  }
 
-	private boolean isExistClaim (TokenMetadata tokenMetadata) {
-		return tokenMetadata.isExistClaim();
-	}
+  private Date getExpiresAt(TokenMetadata tokenMetadata) {
+
+    return tokenMetadata.getExpiresAtOfDateType(LocalDateTime.now());
+  }
+
+  private void addClaimIfPresent(Builder builder, TokenMetadata tokenMetadata) {
+
+    if (isExistClaim(tokenMetadata)) {
+      builder.withClaim(tokenMetadata.claim(), tokenMetadata.userKey());
+    }
+
+  }
+
+  private boolean isExistClaim(TokenMetadata tokenMetadata) {
+
+    return tokenMetadata.isExistClaim();
+  }
 
 }
