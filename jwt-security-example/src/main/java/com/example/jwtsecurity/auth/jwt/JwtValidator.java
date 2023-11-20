575273068d4c49ce7258fec69d5b165801edcbf1
bmm522 <rlawldls11@naver.com>
2023-11-20 21:36:46 +0900

    test : jwtValidator test 추가

--- jwt-security-example/src/main/java/com/example/jwtsecurity/auth/jwt/JwtValidator.java
+++ jwt-security-example/src/main/java/com/example/jwtsecurity/auth/jwt/JwtValidator.java
@@ -1,24 +1,34 @@
 package com.example.jwtsecurity.auth.jwt;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.example.jwtsecurity.auth.jwt.token.Token;
+import com.example.jwtsecurity.auth.jwt.token.TokenDecoder;
+import com.example.jwtsecurity.auth.jwt.token.TokenIssuer;
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@RequiredArgsConstructor
 public class JwtValidator {
 
+    private final TokenDecoder tokenDecoder;
 
-        public void validate() {
-                System.out.println("jwt validate");
+
+    public void validate() {
+        System.out.println("jwt validate");
+    }
+
+    public boolean isExpiredToken(final Token token, final String secretKey) {
+        try {
+            tokenDecoder.decodedJWT(token, secretKey);
+            return false;
+        } catch (TokenExpiredException e) {
+            return true;
         }
-
-        public boolean isExpiredAccessToken(final Token accessToken, final String secretKey) {
-                return true;
-        }
-
-        public boolean isExpiredRefreshToken(final Token refreshToken, final String secretKey) {
-                return false;
-        }
-
+    }
 
 }
--- jwt-security-example/src/test/java/com/example/jwtsecurity/auth/jwt/JwtValidatorTest.java
+++ jwt-security-example/src/test/java/com/example/jwtsecurity/auth/jwt/JwtValidatorTest.java
@@ -3,62 +3,93 @@ package com.example.jwtsecurity.auth.jwt;
 
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.jwtsecurity.auth.jwt.fixture.FixtureToken;
 import com.example.jwtsecurity.auth.jwt.fixture.FixtureTokenMetadata;
 import com.example.jwtsecurity.auth.jwt.token.Token;
+import com.example.jwtsecurity.auth.jwt.token.TokenDecoder;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import org.mockito.junit.jupiter.MockitoExtension;
+@ExtendWith(MockitoExtension.class)
 public class JwtValidatorTest {
 
 
+    @InjectMocks
     JwtValidator jwtValidator;
 
+    @Mock
+    TokenDecoder tokenDecoder;
+
+    String secret;
 
 
     @BeforeEach
     void setUp() {
-        jwtValidator = new JwtValidator();
+        secret = FixtureTokenMetadata.SECRET;
+    }
 
+
+    @Nested
+    @DisplayName("토큰이")
+    class TokenExpirationValidationTests {
+        @ParameterizedTest(name = "parameterInfo: {1}")
+        @MethodSource("provideExpiredTokens")
+        @DisplayName("만료되면 true를 반환한다.")
+        void shouldReturnTrueForExpiredTokens(Token token, String parameterInfo) {
+
+            when(tokenDecoder.decodedJWT(any(), any())).thenThrow(TokenExpiredException.class);
+
+            boolean result = jwtValidator.isExpiredToken(token, secret);
+
+            assertThat(result).isTrue();
+        }
+
+        @ParameterizedTest(name = "parameterInfo: {1}")
+        @MethodSource("provideNotExpiredTokens")
+        @DisplayName("만료되지 않으면 false를 반환한다.")
+        void shouldReturnFalseForNonExpiredTokens(Token token, String parameterInfo) {
+
+            when(tokenDecoder.decodedJWT(any(Token.class), anyString())).thenReturn(null);
+
+            boolean result = jwtValidator.isExpiredToken(token,secret);
+
+            assertThat(result).isFalse();
+        }
+
+        private static Stream<Arguments> provideExpiredTokens() {
+            return Stream.of(
+                Arguments.of(FixtureToken.createAccessTokenByExpirationTime(-10000000L), "만료된 AccessToken"),
+                Arguments.of(FixtureToken.createRefreshTokenByExpirationTime(-10000000L), "만료된 RefreshToken")
+            );
+        }
+
+        private static Stream<Arguments> provideNotExpiredTokens() {
+            return Stream.of(
+                Arguments.of(FixtureToken.createAccessTokenByExpirationTime(10000000L), "만료되지 않은 AccessToken"),
+                Arguments.of(FixtureToken.createRefreshTokenByExpirationTime(10000000L), "만료되지 않은 RefreshToken")
+            );
+        }
     }
 
 
 
-    @Test
-    @DisplayName("Access 토큰이 만료되면 true를 반환한다.")
-    void test1() {
-        Token expiredAccessToken = FixtureToken.createAccessTokenByExpirationTime(-10000000L);
 
-        boolean result = jwtValidator.isExpiredAccessToken(expiredAccessToken, FixtureTokenMetadata.SECRET);
-        assertThat(result).isTrue();
-    }
 
-    @Test
-    @DisplayName("Access 토큰이 만료되지 않으면 false를 반환한다.")
-    void test2() {
-        Token notExpiredAccessToken = FixtureToken.createAccessTokenByExpirationTime(10000000L);
-
-        boolean result = jwtValidator.isExpiredAccessToken(notExpiredAccessToken, FixtureTokenMetadata.SECRET);
-        assertThat(result).isFalse();
-    }
-//
-//    @Test
-//    @DisplayName("토큰이 만료되지 않으면 false를 반환한다.")
-//    void test2() {
-//        Token notExpiredToken = AccessToken.of(JWT.create()
-//            .withSubject(subject)
-//            .withExpiresAt(new Date(System.currentTimeMillis() + 1000000L))
-//            .withClaim(claimName, userKey)
-//            .sign(Algorithm.HMAC256(secret)));
-//
-//        boolean result = jwtAuthenticationValidator.isExpiredJwtToken(notExpiredToken);
-//        assertThat(result).isFalse();
-//    }
-//
-//    @Test
-//    @DisplayName("유효하지 않은 토큰이면 예외를 반환한다.")
-//    void test3 () {
-//
-//    }
 }
