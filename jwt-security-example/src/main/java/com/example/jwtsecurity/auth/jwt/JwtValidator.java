7de9843e1e6c2c2868e6469e76822a12c71f045f
bmm522 <rlawldls11@naver.com>
2023-11-22 00:04:00 +0900

    refactor,test : decoder에서 만료시간 체크하도록 수정 및 테스트 코드 추가

--- jwt-security-example/src/main/java/com/example/jwtsecurity/auth/jwt/JwtValidator.java
+++ jwt-security-example/src/main/java/com/example/jwtsecurity/auth/jwt/JwtValidator.java
@@ -1,12 +1,7 @@
 package com.example.jwtsecurity.auth.jwt;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.example.jwtsecurity.auth.jwt.token.Token;
 import com.example.jwtsecurity.auth.jwt.token.TokenDecoder;
-import com.example.jwtsecurity.auth.jwt.token.TokenIssuer;
-import java.util.Date;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -17,18 +12,8 @@ public class JwtValidator {
 
     private final TokenDecoder tokenDecoder;
 
-
-    public void validate() {
-        System.out.println("jwt validate");
-    }
-
-    public boolean isExpiredToken(final Token token, final String secretKey) {
-        try {
-            tokenDecoder.decodedJWT(token, secretKey);
-            return false;
-        } catch (TokenExpiredException e) {
-            return true;
-        }
+    public boolean isTokenExpired(final Token token, final String secretKey) {
+       return tokenDecoder.isTokenExpired(token, secretKey);
     }
 
 }
